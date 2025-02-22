261790a4eb43ee15230c4898bc773a34f43f7e36
108nitish <166498889+108nitish@users.noreply.github.com>
2025-02-22 20:57:27 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose"
 
 export const connectDB = async()=>{
-    await mongoose.connect(process.env.MONGODB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    })
+    await mongoose.connect(process.env.MONGODB)
         .then(()=>{
             console.log("connected to db")
         })
